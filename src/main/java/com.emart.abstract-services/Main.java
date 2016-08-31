58c49a1c41624c04e004be3bf28a4b96a2e46759
Martin Eberl <martin.eberl@styria.com>
2016-08-31 14:55:43 +0200

    Init Commit

---
+++ src/main/java/com.emart.abstract-services/Main.java
@@ -0,0 +1,13 @@
+package com.emart.application;
+
+import java.util.Properties;
+
+/**
+ * Created by eberlmar on 31.08.16.
+ */
+public class Main {
+
+    public static void main(String[] arg) {
+        Properties properties = new Properties();
+    }
+}
